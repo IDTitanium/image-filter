9ec88af8915a93e7ecb515e852ae70d3080846b0
idris lawal <idriseun222@gmail.com>
2021-10-10 23:30:33 +0100

    update

--- src/util/util.ts
+++ src/util/util.ts
@@ -12,14 +12,14 @@ export async function filterImageFromURL(inputURL: string): Promise<string>{
     return new Promise( async resolve => {
         try{
             const photo = await Jimp.read(inputURL);
-        const outpath = '/tmp/filtered.'+Math.floor(Math.random() * 2000)+'.jpg';
-        await photo
-        .resize(256, 256) // resize
-        .quality(60) // set JPEG quality
-        .greyscale() // set greyscale
-        .write(__dirname+outpath, (img)=>{
-            resolve(__dirname+outpath);
-        })
+            const outpath = '/tmp/filtered.'+Math.floor(Math.random() * 2000)+'.jpg';
+            await photo
+            .resize(256, 256) // resize
+            .quality(60) // set JPEG quality
+            .greyscale() // set greyscale
+            .write(__dirname+outpath, (img)=>{
+                resolve(__dirname+outpath);
+            })
         }
         catch(error) {
             console.error("An error occured while reading file")
